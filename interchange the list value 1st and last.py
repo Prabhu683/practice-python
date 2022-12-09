6b5531a2a9e4b37e272ad0100bd7edb4a2d90fc7
prabhu <sprabhu683@gmail.com>
2022-12-09 19:49:30 +0530

    practice

---
+++ interchange the list value 1st and last.py
@@ -0,0 +1,79 @@
+#interchange 1st and last:
+##a=[1,2,3,4]
+##a[0],a[-1]=a[-1],a[0]
+##print("interchange list:",a)
+
+
+
+##  Input : List = [23, 65, 19, 90], pos1 = 1, pos2 = 3
+##Output : [19, 65, 23, 90]
+##
+##Input : List = [1, 2, 3, 4, 5], pos1 = 2, pos2 = 5
+##Output : [1, 5, 3, 4, 2]  
+##list1,list2=[23,65,19,90],[1,2,3,4,5]
+##
+##list1[0],list1[2]=list1[2],list1[0]
+##
+##list2[1],list2[4]=list2[4],list2[1]
+##
+##print(list1,list2)
+
+####length of list
+##num=[1,2,3,4,5]
+##count=0
+##for i in  num: 
+##    count+=1
+##print("length of list:",count)
+
+##list = test_list = [1, 6, 3, 5, 3, 4]
+##Input: 3  # Check if 3 exist or not.
+##Output: True
+##n=int(input("enter the input:"))
+##List=[1,6,3,5,3,4]
+##if n in List:
+##    print("true")
+##else:
+##    print("false")
+
+#clear the list
+##num=[1,2,3,4,5]
+##num.clear()
+##print(num)
+
+#reverse the list
+##num=[1,2,3,4,5]
+##print("reverse list:",num[::-1])
+
+##sums of the lists
+##Input: [12, 15, 3, 10]
+##Output: 40
+##value=[12,15,3,10]
+##total=0
+##for i in value:
+##    total+=i
+##print("sum of the list:",total)
+
+#multiply the list value:
+##value=[12,15,3,10]
+##total=1
+##for i in value:
+##    total*=i
+##print("sum of the list:",total)
+
+#even num in the list
+##Input: list1 = [2, 7, 5, 64, 14]
+##Output: [2, 64, 14]
+
+##list1=[2,5,7,64,14]
+##for i in list1:
+##    if i%2==0:
+##        print(i,end=",")
+
+#odd num in the list
+##Input: list1 = [2, 7, 5, 64, 14]
+##list1=[2,5,7,64,14]
+##for i in list1:
+##    if i%2!=0:
+##        print(i,end=",")      
+
+
